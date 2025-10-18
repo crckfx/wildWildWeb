72c2be19a92b00634bd51ced4a311a55a6eb18a5
crckfx <crckfx@outlook.com>
2025-10-18 11:31:23 +1100

    resolve numbin paths after fixing its touch scrolling

--- js/global.js
+++ js/global.js
@@ -5,29 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
     loadTheme();
 
     // init any numbins
-    document.querySelectorAll('input.numbin').forEach(input => {
-        const min = parseInt(input.getAttribute('min'), 10) || 0;
-        const max = parseInt(input.getAttribute('max'), 10) || 9999;
-        const step = parseInt(input.getAttribute('step'), 10) || 1;
-
-        let loop = false;
-        if (input.dataset.loop) loop = true;
-
-        new Numbin(input, { min, max, step, loop });
+    document.querySelectorAll('.numbin').forEach(div => {
+        new Numbin(div, {
+            min: +div.dataset.min || 0,
+            max: +div.dataset.max || 9999,
+            step: +div.dataset.step || 1,
+            loop: div.dataset.loop === 'true'
+        });
     });
 
-	const html = document.documentElement;
-	const numbin = document.querySelector("#genColorPicker input.numbin");
+    const html = document.documentElement;
+    const gen_h_numbin = document.querySelector("#genColorPicker .numbin");
 
     // set it up anyways whatever the theme
-    if (numbin) {
-		numbin.addEventListener("input", () => {
-			const val = numbin.value; // we actually don't want to safeguard here; it's up to numbin
-			html.style.setProperty("--gen-h-1", val); // replace this with theme interactivity later
-		});
+    if (gen_h_numbin) {
+        const numbinput = gen_h_numbin.querySelector('input');
+        numbinput.addEventListener("input", () => {
+            const val = numbinput.value; // we actually don't want to safeguard here; it's up to numbin
+            html.style.setProperty("--gen-h-1", val); // replace this with theme interactivity later
+        });
         // read computed value of --gen-h-1
         const h = getComputedStyle(html).getPropertyValue("--gen-h-1").trim();
-        if (h) numbin.value = parseFloat(h);
+        if (h) numbinput.value = parseFloat(h);
     }
-    
+
 });
