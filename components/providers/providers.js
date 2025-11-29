24bc2cdc10234f8f5d278518673425cbe016daff
crckfx <crckfx@outlook.com>
2025-11-29 12:21:31 +1100

    use a graft as a contentPath candidate

--- components/providers/providers.js
+++ components/providers/providers.js
@@ -5,16 +5,15 @@ import { renderMenuHTML } from "./renderMenuHTML.js";
 export function generateMenuHTML(plan) {
 
     // console.log(plan.dist);
-    
-    const flat = buildFlatMap(plan);        // 1. Build flat URL universe
 
+    const flat = buildFlatMap(plan);        // 1. Build flat URL universe
     // // TEMP logging
     // console.log("[header6] URL count:", Object.keys(flat).length);
-    
     const tree = buildTree(flat);           // 2. Build hierarchical tree
     
     // TEMP logging
     // console.log("[header6] root:", tree?.path);
+    
     console.log("[header6] first-level:", tree.children.map(c => c.path));
     
     const filtered = filterMenuTree(tree);  // 3. filter according to populateMenu rules
